f8170c2209739f1c2c630b7f91386967acadb97b
Sakib Fakir <fakirsakib22232>
2025-01-22 16:47:04 +0600

    booking class done

--- src/Home/FeautredClasses.jsx
+++ src/Home/FeautredClasses.jsx
@@ -4,27 +4,37 @@ import React, { useEffect } from "react";
 import Top6bookingClass from "@/Hooks/Top6bookingClass";
 
 function FeautredClasses() {
-
-
-  const {isLoading, top6class} = Top6bookingClass();
+  const { isLoading, top6class } = Top6bookingClass();
   console.log(top6class, "class");
 
   // title des ,booking
 
-  if(isLoading)
-  {
-    return <p>Loading.....</p>
+  if (isLoading) {
+    return <p>Loading.....</p>;
   }
 
   return (
-    <div>
-      
-      class
-      {top6class.length}
+    <div className="mb-6">
+      <div>
+        <h2>Top booking class</h2>
+      </div>
+
+      <section className="grid md:grid-cols-3 px-4 gap-5">
+      {top6class.map((item, key) => (
+
+<div key={key} className="border px-4 rounded-md bg-gradient-to-t from-blue-400 to-red-400 text-center p-6"> 
+  <p>Class Name : {item.Skill}</p>
+  <p>Experience : {item.Experience}</p>
+  <p>Email : {item.Email}</p>
+  <p>Booking Count : {item.bookingCount || "0"
+  }</p>
+</div>
+))}
+      </section>
 
   
-
-
+    
+      
     </div>
   );
 }
