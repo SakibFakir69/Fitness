28667cda4fe2b908e1d7499acf8c13b5987fe44c
Sakib Fakir <FakirSakib22232@gmail.com>
2025-02-18 20:10:28 +0600

    new post done

--- src/Home/ForumPage.jsx
+++ src/Home/ForumPage.jsx
@@ -6,23 +6,30 @@ function ForumPage() {
   const { communityPost } = UseTop6communityPost();
   console.log(communityPost);
 
+
   return (
-    <div>
+    <div className="py-10">
     
-      <h2 className="text-3xl font-semibold text-center">Most recent post</h2>
+   
       <div className="mt-6 px-4 grid sm:grid-cols-2 gap-4">
+
         {communityPost?.map((item, key) => (
-          <div key={key} className="border p-4 rounded-md bg-green-200">
-            <div className="flex gap-4">
-              <div className="flex-1  borde-2">
-                <img src={item.Image} className="rounded-md" />
+          
+          <div key={key} className="border p-4 rounded-md bg-stone-200 shadow-xl hover:shadow-slate-100/50">
+
+            <div className="">
+              <div className="h-64 borde-2 w-full border-2 sm:p-3">
+                <img src={item.Image} className="rounded-md h-60 w-full object-cover" />
               </div>
-              <div>
-                <h2>{item.Title}</h2>
-                <p>{item.Message}</p>
+
+              <div className="h-24 flex flex-col justify-center">
+                <h2 className="text-xl font-semibold">{item.Title}</h2>
+                <p className="text-sm text-stone-800">{item.Message}</p>
               </div>
             </div>
           </div>
+
+
         ))}
       </div>
     </div>
