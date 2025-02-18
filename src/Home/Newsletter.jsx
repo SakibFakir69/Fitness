b43f945120cc3c5bb86b42f9f07e506aaa60f139
Sakib Fakir <FakirSakib22232@gmail.com>
2025-02-18 20:35:02 +0600

    news leter done

--- src/Home/Newsletter.jsx
+++ src/Home/Newsletter.jsx
@@ -36,24 +36,28 @@ function Newsletter() {
   
   return (
     <div className="px-10 mb-6">
-      <div className="px-10 rounded-md border  flex-col bg-gradient-to-b from-fuchsia-700  to-blue-600 ">
-        <div className="text-center p-4">
-          <h1 className="text-4xl font-semibold text-white ">
+      <div className="px-10 rounded-md    bg-gradient-to-b from-fuchsia-700  to-blue-600 flex  md:flex-row flex-col">
+
+        <div className="text-center  flex-1 flex items-center flex-col">
+          <h1 className="text-2xl font-semibold text-white justify-center md:py-8 mt-6">
             {" "}
             Stay Updated with Our Newsletter
           </h1>
-          <p className="text-gray-400">
+          <p className="text-gray-300 text-sm md:-mt-6">
             {" "}
             Subscribe to our newsletter and never miss updates. Just provide
             your name and email to join our community.
           </p>
         </div>
-        <form className="flex flex-col space-y-3" onSubmit={handelSubmitNewesLetter}>
-          <input
+
+        <form className="flex gap-1 justify-center items-center flex-col flex-1 w-full " onSubmit={handelSubmitNewesLetter}>
+
+         <div className=" w-10/12 flex flex-col space-y-6 py-8 md:mt-6">
+         <input
             type="text"
             name="name"
             placeholder="Enter your name"
-            className="text-center rounded-md py-2"
+            className="text-center rounded md:py-3 px-12 py-2"
           />
 
           {/* <input type="text" /> */}
@@ -61,10 +65,13 @@ function Newsletter() {
             type="email"
             name="email"
             placeholder="Enter your email"
-            className="text-center rounded-md py-2"
+            className="text-center rounded md:py-3 py-2"
           />
+         </div>
           <div className="flex justify-center w-full">
-            <button type="submit" className="btn w-1/2 btn-primary text-xl mb-6">
+            <button type="submit" className="btn w-1/2  text-xl mb-6  bg-teal-200/10
+            
+            text-white border-white/30 hover:bg-black">
               Subscribe Now
             </button>
           </div>
